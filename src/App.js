040cf9d26ef008b6d0afd6bea211014291ede6c9
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2023-04-08 15:42:52 +0700

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import { useState } from "react"
 
 function App() {
+  let [work, setWork] = useState('')
   return (
-    <div className="App">
-      app
+    <div className="flex gap-2 justify-center border border-red-500 h-screen items-center">
+      <input onChange={e => setWork(e.target.value)} value={work} className="outline-none border border-blue-600 px-4 py-2" type="text" />
+      <button className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white" type="button">Add</button>
     </div>
   );
 }
